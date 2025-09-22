8e66850bb2901278588501f4fd856590227d76a3
Prayash <128598673+Prayash8@users.noreply.github.com>
2025-09-21 21:50:05 -0400

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("Welcome to Kubia v0.2" + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
